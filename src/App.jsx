971e5e7a2a674453974673b0e31cc9bfe9c3eef2
nnadya <n.d.niki2012@gmail.com>
2026-01-30 19:20:28 +0300

    add AppContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-import { Header } from './components/Header';
-import { ProductList } from './components/ProductList';
-import { Cart } from './components/Cart';
+import { AppContent } from './components/AppContent';
 
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app">
-        <Header />
-        <div className="main-content">
-          <ProductList />
-          <Cart />
-        </div>
-      </div>
+      <AppContent />
     </Provider>
   )
 }
